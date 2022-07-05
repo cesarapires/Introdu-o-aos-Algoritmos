bd01adf1fcc3a1350fc97366851097cd4af2d1de
Cesar A. Pires <cesar.augusto.pires@hotmail.com>
2022-07-05 19:21:02 -0300

    Sol: Lista de estudos - Estruturas Condicionais - Questão 11

--- Lista de Estudos - Estruturas Condicionais/Q11.cpp
+++ Lista de Estudos - Estruturas Condicionais/Q11.cpp
@@ -6,19 +6,19 @@ int main () {
     int matricula;
     int notaFinal;
     int assiduidade;
-    string conceito;
+    char conceito;
 
     cin >> matricula;
     cin >> notaFinal;
     cin >> assiduidade;
 
     if ((assiduidade >= 75 and notaFinal >= 60) or (assiduidade >= 65 and notaFinal >= 70)) {
-        conceito = "A";
+        conceito = 'A';
     } else {
-        conceito = "R";
+        conceito = 'R';
     }
 
-    cout << matricula % 20000000 << conceito << endl;
+    cout << matricula %100000 << conceito << endl;
 
     return 0;
 }
